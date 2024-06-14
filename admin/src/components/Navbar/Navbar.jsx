d37e93aa1b92a79103f2b7112b4c7dd7d61366e2
GillolaHemaVardhanReddy <gillolahemavardhanreddy@gmail.com>
2024-06-14 22:53:41 +0530

    updated navbar regarding home button and logout redux

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -2,29 +2,18 @@ import React from 'react'
 import './Navbar.css'
 import { useNavigate } from 'react-router-dom';
 import nav_log from '../../assets/logos/png/logo-no-background.png'
-import { logOutAndClear, logoutStart, logoutSuccess } from '../../redux/features/auth.slice';
-import { useDispatch, useSelector } from 'react-redux'
-import axios from 'axios';
-import { clearState } from '../../redux/features/product.slice'
-import { analyticsProductClear } from '../../redux/features/analytics.slice';
+import { logOutAndClear } from '../../redux/features/auth.slice';
+import { useDispatch } from 'react-redux'
 
 const Navbar = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const handleLogout = ()=>{
-    dispatch(logoutStart())
     try{
       dispatch(logOutAndClear())
-      // const resp = axios('/auth/signout')
-      // console.log(resp)
-      // dispatch(logoutSuccess())
-      // dispatch(clearState())
-      // dispatch(analyticsProductClear())
-      // navigate('/admin/auth')
     }catch(err){
       console.log(err)
     }
-    // localStorage.removeItem('isAuth')
   }
   return (
     <div className='nav-container'>
@@ -32,7 +21,7 @@ const Navbar = () => {
             <img src={nav_log} alt='' className='nav-logo'/>
         </div>
         <div className='nav-right'>
-            <p className='nav-btn'>Home</p>
+            <p className='nav-btn' onClick={()=>navigate('/admin/manage')}>Home</p>
             <p className='nav-btn' onClick={handleLogout}>Logout</p>
         </div>
     </div>
